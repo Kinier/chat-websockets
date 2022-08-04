da8bfc36f638ab80890129e930ddb5f9e8b30b85
kinier <govormaxsim@gmail.com>
2022-08-04 18:55:19 +0300

    deleted unused imports

--- react-frontend/src/components/SideBar.js
+++ react-frontend/src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react"
+import React from "react"
 
 
 function SideBar({username: username, setUsername: setUsername}) {
@@ -16,7 +16,9 @@ function SideBar({username: username, setUsername: setUsername}) {
                         <a href="#" id={"username_change"} className=" flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                         <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
                             <span className="flex-1 ml-3 whitespace-nowrap m-2">Your name</span>
-                            <input type="text" className="w-full bg-slate-600" onChange={(e)=>{setUsername(e.target.value) }} value={username}></input>
+                            <input type="text" className="w-full bg-slate-600" onChange={(e)=>{setUsername(e.target.value) }} value={username}>
+
+                            </input>
                         </a>
                     </li>
                     
